a77cc4dd50d0a7097f5bb6f0e5b608d12193571a
KonstantinPetrovich++ <klonatoria86.2@mail.ru>
2024-03-22 12:22:26 +0800

    Tenth commit

--- 3.6 Preprocessor and macros/1 Preprocessor directives/Preprocessor directives/Preprocessor directives/Preprocessor directives.cpp
+++ 3.6 Preprocessor and macros/1 Preprocessor directives/Preprocessor directives/Preprocessor directives/Preprocessor directives.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include"Function.h"
 #define MODE 1
 int main() {
 	setlocale(LC_ALL, "Russian");
@@ -10,13 +9,14 @@ int main() {
 #if  MODE == 0
 	std::cout << "Работаю в режиме тренировки" << std::endl;
 #elif  MODE == 1
+#include"Function.h"
 	std::cout << "Работаю в боевом режиме\nВведите число 1: ";
 	std::cin >> x;
 	std::cout << "Введите число 2: ";
 	std::cin >> y;
 	std::cout << "Результат сложения: " << add(x, y) << std::endl;
 #else
-	std::cout << "Неизвестный режим. Завершение работы";
+	std::cout << "Неизвестный режим. \nЗавершение работы";
 #endif
 #endif
 	return 0;
